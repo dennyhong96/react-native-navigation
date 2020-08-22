a8f60a7e71fb2fa8af95195826a1da06126863d1
Haiyang Hong <hong961127@gmail.com>
2020-08-21 19:53:52 -0700

    Add navigation from Feed to product

--- src/stacks/HomeStack.js
+++ src/stacks/HomeStack.js
@@ -1,14 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, FlatList, Button } from "react-native";
+import faker from "faker";
 
 import { AuthContext } from "../AuthProvider";
 import Center from "../components/Center";
 
-const Feed = () => {
+const Feed = ({ navigation }) => {
   return (
     <Center>
-      <Text>Feed</Text>
+      <FlatList
+        style={{ width: "100%" }}
+        keyExtractor={(product, idx) => `${product}-${idx}`}
+        data={Array.from(Array(50), () => faker.commerce.product())}
+        renderItem={({ item }) => (
+          <Button
+            title={item}
+            onPress={
+              () => navigation.navigate("Product", { productName: item }) // Pass params to another screen
+            }
+          />
+        )}
+      />
+    </Center>
+  );
+};
+
+const Product = ({ route, navigation }) => {
+  // Get passed params from route.params (usually id)
+  const productName = route.params.productName;
+  return (
+    <Center>
+      <Text>{productName}</Text>
+      <Button
+        title="Edit this product"
+        onPress={() => navigation.navigate("EditProduct", { productName })}
+      />
+    </Center>
+  );
+};
+
+const EditProduct = ({ route, navigation }) => {
+  // navigation.setOptions({});
+
+  const submit = useRef();
+  submit.current = () => {
+    // Store the submit Api call in to ref.current
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    // Pass into param so we can call it in Screen options
+    navigation.setParams({ submit });
+  }, []);
+
+  // Get passed params from route.params (usually id)
+  return (
+    <Center>
+      <Text>{route.params.productName}</Text>
     </Center>
   );
 };
@@ -30,6 +79,33 @@ const HomeStack = () => {
             </TouchableOpacity>
           ),
         }}
+      />
+      <Stack.Screen
+        options={({ route, navigation }) => ({
+          // Change headerTitle dynamically
+          headerTitle: `Product: ${route.params.productName}`,
+        })}
+        name="Product"
+        component={Product}
+      />
+      <Stack.Screen
+        options={({ route, navigation }) => ({
+          headerTitle: `Edit: ${route.params.productName}`,
+          // Customize header right
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                // Submit form api call
+                // if (route.params.submit) route.params.submit();
+                route.params.submit?.current();
+              }}
+            >
+              <Text style={{ marginRight: 10, color: "red" }}>Done</Text>
+            </TouchableOpacity>
+          ),
+        })}
+        name="EditProduct"
+        component={EditProduct}
       />
     </Stack.Navigator>
   );
