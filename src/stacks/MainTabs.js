87a1648e69539e7dd0dd067840808ef10b3b11ca
Haiyang Hong <hong961127@gmail.com>
2020-08-21 18:13:33 -0700

    Add icons to MainTabs

--- src/stacks/MainTabs.js
+++ src/stacks/MainTabs.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Text, Button } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign, Feather } from "@expo/vector-icons";
 
 import { AuthContext } from "../AuthProvider";
 import Center from "../components/Center";
@@ -27,7 +28,21 @@ const Tab = createBottomTabNavigator();
 
 const MainTabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return <AntDesign name="home" size={size} color={color} />;
+          } else if (route.name === "Search") {
+            return <Feather name="search" size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
